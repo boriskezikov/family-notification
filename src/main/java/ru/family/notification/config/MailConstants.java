b0a6182d721b37f6b65d4c4790e3f7b5f4617278
boriskezikov <boris200898@icloud.com>
2020-09-13 15:24:20 +0300

    update mail

--- src/main/java/ru/family/notification/config/MailConstants.java
+++ src/main/java/ru/family/notification/config/MailConstants.java
@@ -1,6 +1,6 @@
 package ru.family.notification.config;
 
 public interface MailConstants {
-    String PASSWORD = "boris200898Q";
-    String USERNAME = "app.green@mail.ru";
+    String PASSWORD = "Boriskezikov1";
+    String USERNAME = "app.family@mail.ru";
 }
